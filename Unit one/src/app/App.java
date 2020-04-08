ba09954522ae96659e96d4bec4bba48fd79a834f
Matthew-Bartos <817331@mystma.org>
2020-04-08 18:35:58 +0000

    top 5 movies

--- Unit one/src/app/App.java
+++ Unit one/src/app/App.java
@@ -1,7 +1,12 @@
 package app;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(final String[] args) throws Exception {
         System.out.println("Hello Java");
+        System.out.print("1. midway"); 
+        System.out.print("2. lord of the rings");
+        System.out.print("3. 10 commandments");
+        System.out.print("4. Endgame");
+        System.out.print("5. Jumanji");
     }
 }
