e4dc0ce58906ed60c32c6541f599e60bafbc3d70
Matthew-Bartos <817331@mystma.org>
2020-04-09 17:11:08 +0000

    code for 4/9

---
+++ Unitone/src/app/App.java
@@ -0,0 +1,36 @@
+package app;
+
+public class App {
+    public static void main( String[] args) throws Exception {
+        System.out.println("Hello Java");
+        System.out.println("1. midway"); 
+        System.out.println("2. lord of the rings");
+        System.out.println("3. 10 commandments");
+        System.out.println("4. Endgame");
+        System.out.println("5. Jumanji");
+        int myFavNum = 5;
+        boolean livesInStMichael = true;
+        char firstLetterOfName = 'M';
+        byte howOldAmI = 16;
+        short whatYearCar = 2007;
+        long bigBoyNumber = 32153267;
+        float deciSmall = 3.1415f;
+        double bigDecimal = 7.57483243d;
+        System.out.println("whats my fave number");
+        System.out.println(myFavNum);
+        System.out.println("do live in St michael");
+        System.out.println(livesInStMichael);
+        System.out.println("what does my name start with");
+        System.out.println(firstLetterOfName);
+        System.out.println("how old am i");
+        System.out.println(howOldAmI);
+        System.out.println("What year was my car made");
+        System.out.println(whatYearCar);
+        System.out.println("think of a big number");
+        System.out.println(bigBoyNumber);
+        System.out.println("whats the fist 4 digits of pi");
+        System.out.println(deciSmall);
+        System.out.println("how about random decimal");
+        System.out.println(bigDecimal);
+    }
+}
