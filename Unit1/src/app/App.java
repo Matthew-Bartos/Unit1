3a146cc900a351cf61410c1f1fd2716e54a99b60
Matthew-Bartos <817331@mystma.org>
2020-04-15 16:23:24 +0000

    counting

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -4,27 +4,45 @@ public class App {
     public static void main( String[] args) throws Exception {
         System.out.println("Hello Java");
 
-        System.out.println("while loops");
-        int count = 0;
-        String color = "blue";
+        System.out.println("for loops");
+        int count =0;
 
-        while (count < 10) {
+        while (count <= 10){
             System.out.println("the count is" + count);
             count = count + 1;
+        } 
+    
+        for (int i = 0; i <= 10; i = i +1){
+            System.out.println("the count is " + i);
+            System.out.println("Lets keep counting");
+            
         }
-        System.out.println("count finished");
-        while (color == "red"){
-            System.out.println("the color is" + color);
-            color = "green";
+        System.out.println("that was practice");
 
+        for (int i = 10; i <= 20; i = i +1){
+            System.out.println("the count is " + i);
+            System.out.println("Lets keep counting");
         }
-        boolean livesInWisconsin = true;
-        while (livesInWisconsin == true) {
-            
-            System.out.println("i dont live in wisconsin");
-            livesInWisconsin = (false);
-            System.out.println(livesInWisconsin);
-            
+
+        for (int i = 10; i <= 100; i = i +5){
+            System.out.println("the count is " + i);
+            System.out.println("Lets keep counting");
+        }
+
+        for (int i = 1000; i <= 256000; i = i +1000){
+            System.out.println("the count is " + i);
+            System.out.println("Lets keep counting");
+        }
+
+        for (int i = 11; i <= 12; i = i +1){
+            System.out.println("the count is " + i);
+            System.out.println("Lets keep counting");
+        }
+
+        for (int i = 10; i >= 0; i = i -1){
+            System.out.println("the count is " + i);
+            System.out.println("Lets keep counting");
+        }
+        
         }
     }
-}
