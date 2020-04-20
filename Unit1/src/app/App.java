8cfa70b4d8d739f8c6b01facffb1771f274b96cf
Matthew-Bartos <817331@mystma.org>
2020-04-20 16:16:53 +0000

    if else loops

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,35 +1,17 @@
 package app;
 
 public class App {
-    public static void main( String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-        for (int i = 22; i <= 28; i = i +3){
-            System.out.println(i);
-            
-        }
+        for (int i = 1; i <= 20; i++) {
+            if (i % 4 == 0) {
+                System.out.println(i + " can be divided");
 
-        for (int i = 2; i <= 32; i = i * 2){
-            System.out.println(i);
-        
+            } else {
+                System.out.println(i + "can't be divided");
+            }
         }
+        System.out.println("All finished");
 
-        for (int i = 3; i >= -81; i = i * -i){
-            System.out.println(i);
-            
-        }
-
-        for (int i = 50; i >= 5; i = i -5){
-            System.out.println(i);
-            
-        }
-
-
-        for (int i = 1; i <=9; i = i + 1 ){
-             System.out.println(i * i);
-            System.out.println(i +3);
-            System.out.println(i);
-           
-        }
-       
-        }
     }
+}
