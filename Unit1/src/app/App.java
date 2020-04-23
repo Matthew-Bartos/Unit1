32af9dfeb256d18bdd5454bc8de9aa0563e5e5b9
Matthew-Bartos <817331@mystma.org>
2020-04-23 16:11:24 +0000

    im hungry now

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,28 +3,26 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Start first loop");
+        System.out.println("Double or single digit?");
 
-        for (int i = 20; i >= 0; i--) {
-            if (i == 10) {
-                int x = 1;
-
-                 while (x <= 6)  {
-                System.out.println(i);
-                x++;
+        for (int i = 20; i > 0; i--) {
+            if (i >= 10) {
+               
+                System.out.println(i + " is double digit");
+                
                 }
 
-            } else {
-                System.out.println(i);
+             else {
+                System.out.println(i + " is single digit");
             }
         }
-        System.out.println("End First Loop");
-        System.out.println("Start Second Loop");
+        
+        System.out.println("Nice!");
+        System.out.println("Fast food");
 
-        for (int i = 0; i <= 36; i = i + 4 ) {
-            if (i %6 == 0) {
-                System.out.println(i);
-                System.out.println("Hey!");
+        for (int i = 1; i <= 12; i++ ) {
+            if (i %2 == 0) {
+                System.out.println("Fries");
 
 
 
@@ -32,10 +30,17 @@ public class App {
         }
         
         else{
-                System.out.println(i);
+             System.out.println("Burgers");   
             }
             
     }
-        System.out.println("End Second Loop");
-    }
+        System.out.println("Nice!");
+        
+  
+  
+  
+  
+  
+    }  
 }
+
