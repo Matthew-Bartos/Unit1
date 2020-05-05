1841528960ed8b7c066013a2fc1fa92bb79ddac8
Matthew-Bartos <817331@mystma.org>
2020-05-05 19:37:01 +0000

    quiz

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,23 +1,42 @@
 package app;
 
 import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
 
       Scanner input = new Scanner(System.in); 
 
   
-         System.out.println("Please enter a random number");
-      int v = input.nextInt();
-     for ( int x = 1 ; x <= v; x++){
-          System.out.println(x);
+         System.out.println("Please answer the following quwstions");
+          System.out.println("Is the earth round?");
+      String isTheEarthRound = input.nextLine();
+     if (isTheEarthRound == "yes"){
+          System.out.println("incorrect");
      }
 
-        
+        else{
+            System.out.println("correct");
+        }
             
-      
-    
+       System.out.println("are dogs marsupials?");
+    String areDogsMarsupials = input.nextLine();
+     if (areDogsMarsupials == "yes"){
+          System.out.println("incorrect");
+     }
+
+        else{
+            System.out.println("correct");
+        }
+         System.out.println(" we're in highschool.");
+        String isInHighschool = input.nextLine();
+     if (isInHighschool == "yes"){
+          System.out.println("incorrect");
+     }
+
+        else{
+            System.out.println("correct");
+        }
+             System.out.println("good job");
         
         
   
